73b5f40bbbb549e2f3ab270084726d3ad112ab01
Lasse Kelling <lasse_kelling@icloud.com>
2023-10-26 12:58:24 +0200

    Initialpush

---
+++ Oszilloskop/main.c
@@ -0,0 +1,30 @@
+/**
+ * Name: Lasse Kelling
+ * Matrikelnummer: 2590639
+ * Datum: 26.10.2023
+ *
+ * Unter (Properties -> Resource) encoding UTF-8 und line delimitor Unix auswählen!
+ */
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+
+
+void main(), setup();
+
+void main() {
+    setup();
+
+    while(1) {}
+}
+
+void setup() {
+    printf("Setup erfolgreich!\n");
+}
